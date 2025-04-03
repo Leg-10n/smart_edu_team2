85194f2206904454a7f6f6e97caa6a54fcb03ebc
sumit0412 <sumit.sachdev412@gmail.com>
2025-04-04 03:07:32 +0700

    updated subscriptions_controller_test.rb and fixtures

--- test/controllers/subscriptions_controller_test.rb
+++ test/controllers/subscriptions_controller_test.rb
@@ -1,11 +1,17 @@
+# test/controllers/subscriptions_controller_test.rb
 require "test_helper"
 
 class SubscriptionsControllerTest < ActionDispatch::IntegrationTest
   setup do
     # Log in a user before running tests
     @user = users(:one) # Use an existing fixture
-    # Notice we're using email_address here instead of email
-    post session_url, params: { email: @user.email_address, password: "password" }
+
+    # Match the exact parameter names expected by SessionsController
+    post session_url, params: {
+      email_address: @user.email_address,
+      password: "password"
+    }
+
     @subscription = subscriptions(:one)
   end
 
