2eea05b3bd5343b1c8615ac2deda7235ab62e933
Hir0Yat0 <xToppyThailandx@gmail.com>
2025-04-04 01:34:39 +0700

    feat: change school table's "name" to "school_name"

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,10 +2,10 @@
 #
 # Table name: schools
 #
-#  id         :integer          not null, primary key
-#  name       :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  school_name :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 class School < ApplicationRecord
 end
