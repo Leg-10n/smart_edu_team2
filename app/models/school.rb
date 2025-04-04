eb448d3ca43ecbe08af0ab1e8cdc6cd4867b322b
Hir0Yat0 <xToppyThailandx@gmail.com>
2025-04-04 08:07:46 +0700

    feat: add school name validations

--- app/models/school.rb
+++ app/models/school.rb
@@ -8,4 +8,6 @@
 #  updated_at  :datetime         not null
 #
 class School < ApplicationRecord
+    normalizes :school_name, with: ->(e) { e.strip }
+    validates :school_name, presence: true, uniqueness: true
 end
